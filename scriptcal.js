50360b2ba3ec9a1e5093461cce0317fc223e7e86
Akash kumar <92292782+Akkoakash@users.noreply.github.com>
2022-01-21 13:15:18 +0530

    Add files via upload

---
+++ scriptcal.js
@@ -0,0 +1,20 @@
+var uberClientId = "Akash"
+uberServerToken = "Abc_qrst";
+navigator.geolocation.watchPosition(function(position) {
+    userLatitude = position.coords.latitude;
+    userLongitude = position.coords.longitude;
+ getEstimatesForUserLocation(userLatitude, userLongitude);
+});
+function getEstimatesForUserLocation(start_latitude,start_longitude)
+ { 
+    start_latitude; latitude,
+    start_longitude; longitude,
+    end_latitude; partyLatitude,
+    end_longitude; partyLongitude
+    
+
+  }
+  
+    console.log(result);
+  
+ 
